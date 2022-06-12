d0fe85fd3528ef8305ef4e3e03646de3f34036bc
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-06-12 17:13:02 -0400

    add code to SeasonDisplay component

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+  summer: {
+    text: 'Time for the beach, bitches!',
+    iconName: 'sun',
+  },
+  winter: {
+    text: "Burrr it's cold outside!",
+    iconName: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`${season} season-display`}>
+      <i className={`${iconName} icon massive icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
